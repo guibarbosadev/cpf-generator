b6dc63434d69d0bb2284abbad19deff8d1cbf711
Guilherme Barbosa Pereira <guilherme.pereira@sorteonline.com.br>
2020-08-21 19:01:50 -0300

    Cleanup: 	- Remove unnecessay "console.log" 	- Declare functions before exporting it

--- cpf.js
+++ cpf.js
@@ -1,12 +1,15 @@
 function validatorDigit(CPF) {
     const CPFCopy = [...CPF];
     const initialSumValue = 0;
-    const sum = CPFCopy.reverse().reduce((previousValue, currentValue, index) => {
-        const multiplicator = index + 2;
-        const currentSum = previousValue + currentValue * multiplicator;
+    const sum = CPFCopy.reverse().reduce(
+        (previousValue, currentValue, index) => {
+            const multiplicator = index + 2;
+            const currentSum = previousValue + currentValue * multiplicator;
 
-        return currentSum;
-    }, initialSumValue);
+            return currentSum;
+        },
+        initialSumValue
+    );
     const digit = (sum * 10) % 11;
 
     return digit < 10 ? digit : 0;
@@ -18,25 +21,25 @@ function generateRandomNumbers(numbersToGenerate = 1) {
     });
 }
 
-module.exports = {
-    generateCPF: function (withMask = false) {
-        let CPF = generateRandomNumbers(9);
+function generateCPF(withMask = false) {
+    let CPF = generateRandomNumbers(9);
 
-        CPF.push(validatorDigit(CPF));
-        CPF.push(validatorDigit(CPF));
-        CPF = CPF.join('');
+    CPF.push(validatorDigit(CPF));
+    CPF.push(validatorDigit(CPF));
+    CPF = CPF.join('');
 
-        if (withMask === true) {
-            CPF = maskCPF(CPF);
-        }
-
-        return CPF;
-    },
-
-    maskCPF: function (CPF = '') {
-        const mask = /(\d{3})(\d{3})(\d{3})(\d{2})/;
-        const withMask = CPF.replace(mask, '$1.$2.$3-$4');
-
-        return withMask;
+    if (withMask === true) {
+        CPF = maskCPF(CPF);
     }
+
+    return CPF;
 }
+
+function maskCPF(CPF = '') {
+    const mask = /(\d{3})(\d{3})(\d{3})(\d{2})/;
+    const withMask = CPF.replace(mask, '$1.$2.$3-$4');
+
+    return withMask;
+}
+
+module.exports = { generateCPF, maskCPF };
--- public/index.js
+++ public/index.js
@@ -1,13 +1,11 @@
-console.log('dask');
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('daskodsako');
     const inputs = document.querySelectorAll('input');
+    
     inputs.forEach((input) => {
         input.addEventListener('click', function () {
            this.select();
            this.setSelectionRange(0, 99999);
            document.execCommand('copy');
-           console.log('adskodskoa');
         })
     })
 })
